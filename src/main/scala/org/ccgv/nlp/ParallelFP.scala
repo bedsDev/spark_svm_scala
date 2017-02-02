bd764adf9c1ba75553c8ac1a2da66cbbaf009524
Dennis <nets_beds@gmail.com>
2017-02-02 15:36:46 +0000

    adding div full text data

--- src/main/scala/org/ccgv/nlp/ParallelFP.scala
+++ src/main/scala/org/ccgv/nlp/ParallelFP.scala
@@ -33,8 +33,8 @@ object ParallelFP{
 		println(supportNum)
 
 		val conf = new SparkConf()
+			.set("spark.driver.memory","20g")
 			.setAppName("Parallel Frequent Pattern")
-			.set("spark.driver.memory","6g")
 	    val sc = new SparkContext(conf)
 
 	    /* the decomposition for the term frequencies of the documents */
